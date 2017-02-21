e2cc400b94b36c3a53ed05885203cb4789a20153
Swarit Agarwal <agarwalswarit@gmail.com>
2017-02-22 07:18:25 +0800

    first commit

---
+++ www/js/constants.js
@@ -0,0 +1,6 @@
+angular.module('starter')
+ 
+.constant('API_ENDPOINT', {
+  // url: 'http://localhost:8080'
+  //  For a simulator use: url: 'http://127.0.0.1:8080/api'
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,6 @@
+angular.module('starter')
+
+.controller('YourItemCtrl', function($scope, $state, $http, $interval, $q, $window, $ionicPopup, API_ENDPOINT, $ionicModal) {
+
+
+})
